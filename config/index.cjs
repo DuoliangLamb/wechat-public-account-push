1b463fc95425a6eec2ee20477a4b545d8f49ee83
DuoliangLamb <91953102+DuoliangLamb@users.noreply.github.com>
2023-10-13 22:02:23 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -114,7 +114,7 @@ const USER_CONFIG = {
       id: 'o8jCF6g7OuxlO9zxumhe1Gm8sIYs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-      useTemplateId: 'mhjyLnhowtURvmPwI6iF06IdIMDBN9QfYC9iel8eeJ4',
+      useTemplateId: 'o0TZKGg7LqKzwCcCN5QjzGIvzbsH-A6chlRqR5VzB6M',
       // 所在省份或城市，也可以不填
       province: '上海',
       // 所在城市或县区
